6c6de58cc4b053a24b4500392c81e3d75dfefab9
Akash chaudhary <Akash.chaudhary@codeaegis.in>
2022-01-27 16:45:35 +0530

    changes

--- first.cpp
+++ first.cpp
@@ -1,8 +1,14 @@
+// #include <iostream>
+//
+// using namespace std;
+//
+// int main(){
+//   std::cout << "hello world" << '\n';
+//   return 0;
+// }
 #include <iostream>
-
 using namespace std;
-
-int main(){
+int main(int argc, char const *argv[]) {
   std::cout << "hello world" << '\n';
   return 0;
 }
